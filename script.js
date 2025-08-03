d2851e335cea4386662b0b345f99d9a8ef0bc00e
Thabang <phoshokothabang1@gmail.com>
2025-08-03 10:02:06 +0200

    feat: add an add task function to alert the user to add an item to the list

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
+    function addTask() {
+        let taskText = taskInput.value.trim();
+
+        if (!taskText) {
+            alert('Enter a to-do item in the input field');
+        }
+    }
+
     
 });
