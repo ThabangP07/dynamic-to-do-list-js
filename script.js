3ed62d31f3dcc1f513b09b68d9065037ad34cd4c
Thabang <phoshokothabang1@gmail.com>
2025-08-03 10:30:08 +0200

    chore: add comments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    //access the DOM elements
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
@@ -7,15 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
         let taskText = taskInput.value.trim();
 
         if (!taskText) {
-            alert('Enter a to-do item in the input field');
+            alert('Enter a to-do item in the input field'); // alert the user
         } else {
+            // create a new li element
             const listItem = document.createElement('li');
             listItem.textContent = taskText;
             
+            // create a new button for the li element
             const removeBtn = document.createElement('button');
             removeBtn.textContent = 'Remove';
             removeBtn.className = 'remove-btn';
 
+            // add an event listener to remove the li element
             removeBtn.addEventListener('click', function() {
                 taskList.remove('li');
             })
@@ -23,6 +28,4 @@ document.addEventListener('DOMContentLoaded', function() {
             taskInput.value = "";
         }
     }
-
-    
 });
