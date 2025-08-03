4c41721caf59e931d1f4a549d03e4cc06c316833
Thabang <phoshokothabang1@gmail.com>
2025-08-03 09:55:55 +0200

    feat: add an event listener for the HTML document and access the elements

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    
+});
